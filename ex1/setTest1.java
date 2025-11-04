46efd12bf43dbf6af9983a900c43d1e7b81dd949
Nummer1InfFan <123357877+Nummer1InfFan@users.noreply.github.com>
2025-11-04 13:26:03 +0100

    More tests

--- ex1/setTest1.java
+++ ex1/setTest1.java
@@ -13,8 +13,15 @@ class setTest1 {
         s.insert(5);
         assertArrayEquals(new int[]{5}, s.toArray());
 
+        //Works when we have [4,5,4] but fails with [4,5] (which should work). Bug found and code changed.
+        //See test on SetChanged below
         s.insert(4);
-        assertArrayEquals(new int[]{4, 5}, s.toArray());
+        assertArrayEquals(new int[]{4, 5, 4}, s.toArray());
+
+        //Works when we have [4,5,4, 5] but fails with [4,5, 4] (which should work). Bug found and code changed.
+        //See test on SetChanged below
+        s.insert(5);
+        assertArrayEquals(new int[]{4, 5, 4, 5}, s.toArray());
 
         SetChanged cs = new SetChanged();
         cs.insert(5);
@@ -22,6 +29,13 @@ class setTest1 {
 
         cs.insert(4);
         assertArrayEquals(new int[]{4, 5}, cs.toArray());
+
+        cs.insert(5);
+        assertArrayEquals(new int[]{4, 5}, cs.toArray());
+
+        cs.insert(6);
+        assertArrayEquals(new int[]{4, 5, 6}, cs.toArray());
+
     }
 
     /*
